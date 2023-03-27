c476086ad45816da56e0779f651b8213f116e333
Shubham Kale <shubhamkale1599@gmail.com>
2023-03-27 13:57:05 +0530

    migrated data from sqlite3 to postgres

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -72,13 +72,17 @@ WSGI_APPLICATION = 'jewelryshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'jewelery',
+        "USER": 'postgres',
+        "PASSWORD": 'ShEiNzo@1095',
+        "HOST": 'localhost',
+        "PORT": '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
